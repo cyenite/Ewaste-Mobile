d8662e91b00424adce96742aa944b8d616980f3e
Aaron Kipkoech <aaronrono42@gmail.com>
2020-09-23 11:25:49 +0300

    Implement getDestination

--- app/src/main/java/com/ewaste/collection/activity/RideCarActivity.java
+++ app/src/main/java/com/ewaste/collection/activity/RideCarActivity.java
@@ -396,7 +396,7 @@ public class RideCarActivity extends AppCompatActivity
                 LatLng latLng = place.getLatLng();
                 if (latLng != null) {
                     gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                            new LatLng(-1.352273, 36.902663), 15f)
+                            getShortDestination(), 15f)
                     );
                     onDestination();
                 }
@@ -547,7 +547,7 @@ public class RideCarActivity extends AppCompatActivity
     private void onDestination() {
 
         if (destinationMarker != null) destinationMarker.remove();
-        LatLng centerPos = new LatLng(-1.352273, 36.902663);
+        LatLng centerPos = getShortDestination();
         //LatLng centerPos = gMap.getCameraPosition().target;
         destinationMarker = gMap.addMarker(new MarkerOptions()
                 .position(centerPos)
